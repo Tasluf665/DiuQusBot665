805d532b80b14b80db2d215850c43de7fb623c03
Tasluf665 <tasluf15-12089@diu.edu.bd>
2022-12-26 13:58:45 +0600

    Some imporvement in index.js

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ const express = require("express");
 const app = express();
 const TelegramBot = require("node-telegram-bot-api");
 const allQuestion = require("./questions.json");
-const fs = require("fs");
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/add_question/, (msg) => {
-  bot.sendMessage(
-    msg.chat.id,
-    "Send your question into this email: tasluf665@gmail.com"
-  );
-});
-
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "Welcome to the DIU Questions Bank");
 });
+
 bot.onText(/\/help/, (msg) => {
   bot.sendMessage(
     msg.chat.id,
     "Use your course code and enter the qus type. Ex: /cse112_mid to get the questions"
+  );
+});
+
+bot.onText(/\/add_question/, (msg) => {
+  bot.sendMessage(
+    msg.chat.id,
+    "Go to GitHub: https://github.com/Tasluf665/DiuQusBot665\nOr Send your question into this email: tasluf665@gmail.com"
   );
 });
 
